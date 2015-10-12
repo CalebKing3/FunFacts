379f85d4b9d77148bf0629ebc772780e8aad6c67
Caleb King <CalebKing@LiveNation.com>
2015-10-11 17:26:18 -0700

    Single resposibility refactoring

---
+++ app/src/main/java/com/calebking/funfacts/FactBook.java
@@ -0,0 +1,44 @@
+package com.calebking.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by caleb.king on 10/11/15.
+ */
+public class FactBook {
+
+    // Member variable (properties about the object)
+    public String[] mFacts = {
+            "Ticketmaster is the world's largest ticket distribution company in the United State.",
+            "The company distributes tickets for more than 10,000 clients.",
+            "Tickets are sold at roughly 3,300 outlets worldwide.",
+            "Ticketmaster was started by two Arizona State University students.",
+            "Ticketmaster's strategy changed in 1982, when Chicago investor Jay Pritzker purchased it.",
+            "In 1991 Main rival Ticketron is acquired, giving the company market dominance.",
+            "In 1996 Ticketmaster goes public, launches online, travel, and magazine ventures.",
+            "Ticketmaster merged with Livenation in 2010",
+            "Ticketmaster is awesome!",
+            "Ticketmaster was founded by Albert Leffler and Peter Gadwa and businessman Gordan Gunn"
+    };
+
+    // Method (abilities: things the object can do)
+
+    public String getFact() {
+
+        String fact = "";
+
+        // Randomly select a fact
+        Random randomGenerator = new Random();  // Construct a new Random number generator
+        int randomNumber = randomGenerator.nextInt(mFacts.length);
+
+        /**
+         * Convert the randomNumber to a text fact
+         * 0 = Ants stretch when they wake up in the morning.
+         * 1 = Ostriches can run faster than horses.
+         * 2 = Olympic gold medals are actually made mostly of silver
+         * */
+
+        fact = mFacts[randomNumber];
+        return fact;
+    }
+}
--- app/src/main/java/com/calebking/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/calebking/funfacts/FunFactsActivity.java
@@ -5,14 +5,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.Random;
-
 
 public class FunFactsActivity extends Activity {
+
+    private FactBook mFactBook = new FactBook();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,38 +25,7 @@ public class FunFactsActivity extends Activity {
 
             @Override
             public void onClick(View view) {
-                String[] facts = {
-                        "Ticketmaster is the world's largest ticket distribution company in the United State.",
-                        "The company distributes tickets for more than 10,000 clients.",
-                        "Tickets are sold at roughly 3,300 outlets worldwide.",
-                        "Ticketmaster was started by two Arizona State University students.",
-                        "Ticketmaster's strategy changed in 1982, when Chicago investor Jay Pritzker purchased it.",
-                        "In 1991 Main rival Ticketron is acquired, giving the company market dominance.",
-                        "In 1996 Ticketmaster goes public, launches online, travel, and magazine ventures.",
-                        "Ticketmaster merged with Livenation in 2010",
-                        "Ticketmaster is awesome!",
-                        "Ticketmaster was founded by Albert Leffler and Peter Gadwa and businessman Gordan Gunn"
-                };
-
-                // The button was clicked, so update the fact label with a new fact
-                String fact = "";
-                // Randomly select a fact
-                Random randomGenerator = new Random();  // Construct a new Random number generator
-                int randomNumber = randomGenerator.nextInt(facts.length);
-
-                /**
-                 * Convert the randomNumber to a text fact
-                 * 0 = Ants stretch when they wake up in the morning.
-                 * 1 = Ostriches can run faster than horses.
-                 * 2 = Olympic gold medals are actually made mostly of silver
-                 * */
-
-                fact = facts[randomNumber];
-
-
-
-
-
+                String fact = mFactBook.getFact();
                 // Update the label with our dynamic fact
                 factLabel.setText(fact);
             }
